1efc109e6a1ca99a6f5b194e41604cc72567ce38
jenilparsania <151541448+jenilparsania@users.noreply.github.com>
2024-09-14 19:23:11 -0300

    refactored reset logic (#1)

--- resetpassword.py
+++ resetpassword.py
@@ -3,5 +3,6 @@ password = input("Enter pass: ")
 
 if username == "admin" and password == "admin":
     print("you can reset")
+    print("Adding the line here")
 else:
     print("you cannot reset")
